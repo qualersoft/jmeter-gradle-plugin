3d0c52fd6c20efcde37844d45582ebee4d4b48ee
Mathze <270275+mathze@users.noreply.github.com>
2021-09-06 19:53:39 +0200

    #6 fixed report issue & changelog issue as well

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,24 @@
-<!-- Keep a Changelog guide -> https://keepachangelog.com -->
+<!--
+Types of changes
+ - `Added` for new features.
+ - `Changed` for changes in existing functionality.
+ - `Deprecated` for soon-to-be removed features.
+ - `Removed` for now removed features.
+ - `Fixed` for any bug fixes.
+ - `Security` in case of vulnerabilities.
+Only add when needed
+-->
+
 # jmeter-gradle-plugin Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com),
+and this project adheres to [Semantic Versioning](https://semver.org).
 
 ## [Unreleased]
+### Fixed
+- Run-Task now respects report settings (#6)
 
-## [1.0.1-SNAPSHOT]
+## [1.0.0]
 ### Added
 - Initial version
-
--- src/main/kotlin/de/qualersoft/jmeter/gradleplugin/task/JMeterRunTask.kt
+++ src/main/kotlin/de/qualersoft/jmeter/gradleplugin/task/JMeterRunTask.kt
@@ -1,7 +1,14 @@
 package de.qualersoft.jmeter.gradleplugin.task
 
+import de.qualersoft.jmeter.gradleplugin.CopyResource
+import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.work.DisableCachingByDefault
+import java.io.File
 
 /**
  * Task to execute jmeter through cli mode (no gui).
@@ -21,6 +28,26 @@ open class JMeterRunTask : JMeterBaseTask() {
    */
   @Input
   var generateReport: Boolean = false
+
+  /**
+   * Path to a user defined report template folder.
+   * Must have name `report-template`.
+   *
+   * Defaults to `jmeter.tool.reportTemplateFolder`. (If not set uses the bundled)
+   */
+  @InputDirectory
+  @PathSensitive(PathSensitivity.ABSOLUTE)
+  @Optional
+  val reportTemplate: DirectoryProperty = objectFactory.directoryProperty().value(
+    jmExt.tool.reportTemplateDirectory
+  )
+
+  override fun processResources(jmBinDir: File) {
+    super.processResources(jmBinDir)
+    if (generateReport) {
+      copyReportTemplate(reportTemplate, jmBinDir)
+    }
+  }
 
   override fun createRunArguments() = mutableListOf<String>().also {
     it.add("-n") // no gui
