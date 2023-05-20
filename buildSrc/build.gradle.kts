cca6a7dd93a83f28c1b4fe0907f4b7dc9be52ab2
Mathze <270275+mathze@users.noreply.github.com>
2023-05-20 16:39:35 +0200

    05/23 Update all dependencies
    - bumped gradle wrapper to 8.1.1;
    - bumped kotlin to v1.8.21;
    - bumped testkit to v1.0.12;
    - bumped junit to v5.9.3;
    - bumped kotest to v5.6.2;
    - bumped jacoco tool version to v0.8.10;

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.8.20"
+  kotlin("jvm") version "1.8.21"
 }
 
 repositories {
